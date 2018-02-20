558a2d4588ed420b363a64bbaf554ae98cfb5ab4
Vitaly Kiselev <st411ar@ya.ru>
2018-02-20 21:53:53 +0300

    created article component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import {Component, HostBinding, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  ngOnInit() {}
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
+
+}
